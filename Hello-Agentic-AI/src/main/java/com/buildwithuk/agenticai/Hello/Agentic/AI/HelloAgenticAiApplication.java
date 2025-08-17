90932bb1a4567babeb50d0e631a51551fba4bd49
Umair Khan <umairk83@live.com>
2025-08-17 22:53:22 +0500

    Initial commit

---
+++ Hello-Agentic-AI/src/main/java/com/buildwithuk/agenticai/Hello/Agentic/AI/HelloAgenticAiApplication.java
@@ -0,0 +1,13 @@
+package com.buildwithuk.agenticai.Hello.Agentic.AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAgenticAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAgenticAiApplication.class, args);
+	}
+
+}
